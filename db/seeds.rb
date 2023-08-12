17c6a89d003c74d206f6ed3f929854ec161edd41
Fernando Aponte <faponte1230@yahoo.com>
2023-08-11 20:54:34 -0400

    populate seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+Gym.create(
+    name:'Retro Fitness',
+    location: 'NYC',
+    image_url: 'https://mma.prnewswire.com/media/538065/Retro_Fitness_Logo.jpg?p=facebook',
+    number_of_gyms: 55,
+    membership_price: 30
+)
+
 
 puts "✅ Done seeding!"
