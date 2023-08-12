effe906a296e7cb1bac36e666e4983af12b7bea3
Fernando Aponte <faponte1230@yahoo.com>
2023-08-11 20:41:38 -0400

    populate createReviews migration

--- db/migrate/20230812002219_create_reviews.rb
+++ db/migrate/20230812002219_create_reviews.rb
@@ -1,4 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :title
+      t.string :body
+      t.integer :rating
+      t.integer :gym_id
+    end
   end
 end
