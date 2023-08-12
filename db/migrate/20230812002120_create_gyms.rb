5c7c5091fb6776f6ce01666fb2d95064b9454da8
Fernando Aponte <faponte1230@yahoo.com>
2023-08-11 20:34:25 -0400

    populate createGyms migration

--- db/migrate/20230812002120_create_gyms.rb
+++ db/migrate/20230812002120_create_gyms.rb
@@ -1,4 +1,11 @@
 class CreateGyms < ActiveRecord::Migration[6.1]
   def change
+    create_table :gyms do |t|
+      t.string :name
+      t.string :location
+      t.string :image_url
+      t.integer :number_of_gyms
+      t.integer :membership_price
+    end
   end
 end
