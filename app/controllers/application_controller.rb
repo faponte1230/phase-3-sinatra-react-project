12df27741259263f66eaa5abeb1fd925936f1aab
Fernando Aponte <faponte1230@yahoo.com>
2023-08-11 23:16:35 -0400

    populate controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,15 @@ class ApplicationController < Sinatra::Base
     
   end
 
+  post "/gyms" do
+    gym = Gym.create(
+      name: params[:name],
+      location: params[:location],
+      image_url: params[:image_url],
+      number_of_gyms: params[:number_of_gyms],
+      membership_price: params[:membership_price]
+    )
+    gym.to_json(include: :reviews)
+  end
+
 end
